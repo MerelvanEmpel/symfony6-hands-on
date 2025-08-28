cb19e7f4e4151996ef7ad59476b7a348d2d0b443
Merel <merelvanempel@gmail.com>
2025-08-28 17:21:50 +0200

    Routing using PHP 8 attributes

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,10 +1,19 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 
 class HelloController {
+    private array $messages = ["Hello", "Hi", "Bye!"];
+
+    #[Route('/', name: 'app_index')]
     public function index(): HttpFoundationResponse {
-        return new HttpFoundationResponse("HI!");
+        return new HttpFoundationResponse(implode(',', $this->messages));
+    }
+
+    #[Route('/messages/{id}', name: 'app_show_one')]
+    public function showOne($id): HttpFoundationResponse {
+        return new HttpFoundationResponse($this->messages[$id]);
     }
 }
