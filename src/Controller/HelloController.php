642b69fb7b3e9976845ce67dc48fa29e5c65f48c
Merel <merelvanempel@gmail.com>
2025-08-28 17:26:43 +0200

    Route Parameter Requirements / Optional Parameters

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,19 +1,19 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController {
     private array $messages = ["Hello", "Hi", "Bye!"];
 
-    #[Route('/', name: 'app_index')]
-    public function index(): HttpFoundationResponse {
-        return new HttpFoundationResponse(implode(',', $this->messages));
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    public function index(int $limit): HttpFoundationResponse {
+        return new HttpFoundationResponse(implode(',', array_slice($this->messages, 0, $limit)));
     }
 
-    #[Route('/messages/{id}', name: 'app_show_one')]
-    public function showOne($id): HttpFoundationResponse {
+    #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+    public function showOne(int $id): HttpFoundationResponse {
         return new HttpFoundationResponse($this->messages[$id]);
     }
 }
