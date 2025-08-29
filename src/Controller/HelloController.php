3cbdcd925fd282b97163454b3fe0766ecba6c806
Merel <merelvanempel@gmail.com>
2025-08-29 13:41:15 +0200

    33. Twig Functions - Including Partial Templates

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,7 +6,11 @@ use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController {
-    private array $messages = ["Hello", "Hi", "Bye!"];
+    private array $messages = [
+        ['message' => 'Hello', 'created' => '2025/08/12'], // Must be within the last three months
+        ['message' => 'Hi', 'created' => '2025/06/12'], // Must be within the last three months
+        ['message' => 'Bye!', 'created' => '2021/05/12'] // Must be at least a year ago
+    ];
 
     #[Route('/{limit<\d+>?3}', name: 'app_index')]
     public function index(int $limit): HttpFoundationResponse {
