f24c4f22d08943d38279f908c880537a4ce6f784
Merel <merelvanempel@gmail.com>
2025-09-09 12:38:08 +0200

    74. Hashing Passwords and Creating Users in Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,29 @@
 namespace App\DataFixtures;
 
 use DateTime;
+use App\Entity\User;
 use App\Entity\MicroPost;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher) {
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $user0 = new User();
+        $user0->setEmail('test@test.com');
+        $user0->setPassword($this->userPasswordHasher->hashPassword($user0, 'test'));
+        $manager->persist($user0);
+
+        $user1 = new User();
+        $user1->setEmail('test1@test.com');
+        $user1->setPassword($this->userPasswordHasher->hashPassword($user1, 'test'));
+        $manager->persist($user1);
+
         $microPost0 = new MicroPost();
         $microPost0->setTitle('Welcome to Poland!');
         $microPost0->setText('Welcome to Poland!');
