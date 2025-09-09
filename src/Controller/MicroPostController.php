bfda702e005cffc1e2031ebf30f9b27548aac233
Merel <merelvanempel@gmail.com>
2025-09-09 14:50:09 +0200

    80. Getting The Current User

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -40,6 +40,7 @@ final class MicroPostController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
             $post->setCreated(new DateTime());
+            $post->setAuthor($this->getUser());
             $posts->add($post, true);
 
             // Add a flash message
@@ -88,6 +89,7 @@ final class MicroPostController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $comment = $form->getData();
             $comment->setPost($post);
+            $comment->setAuthor($this->getUser());
             $comments->add($comment, true);
 
             // Add a flash message
