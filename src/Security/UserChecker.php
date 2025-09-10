629869b9ec3f1455dd8a8b605b0ea0f9c649ca13
Merel <merelvanempel@gmail.com>
2025-09-10 17:46:35 +0200

    Fix exception not being shown for login page (probably because the user is redirected to the login page after getting the 403 so the error message is being caught and then hiding in the redirect. Not even Profiler shows any error message being thrown.

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -3,9 +3,9 @@ namespace App\Security;
 
 use DateTime;
 use App\Entity\User;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 
 class UserChecker implements UserCheckerInterface
 {
@@ -20,7 +20,7 @@ class UserChecker implements UserCheckerInterface
 
         $now = new DateTime();
         if ($now < $user->getBannedUntil()) {
-            throw new AccessDeniedHttpException('The user is banned');
+            throw new CustomUserMessageAuthenticationException('The user is banned');
         }
     }
 
