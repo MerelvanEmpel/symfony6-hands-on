19e9d46be728e4f40bd70eb4842b888219746059
Merel <merelvanempel@gmail.com>
2025-09-11 12:02:19 +0200

    90. Embedding Forms

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -5,15 +5,18 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\UserProfileType;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class SettingsProfileController extends AbstractController
 {
     #[Route('/settings/profile', name: 'app_settings_profile')]
-    public function profile(Request $request): Response
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function profile(Request $request, UserRepository $users): Response
     {
         /** @var User $user */
         $user = $this->getUser();
@@ -24,9 +27,11 @@ final class SettingsProfileController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userProfile = $form->getData();
-            // Save
-            // Add the flash message
-            // Redirect
+            $user->setUserProfile($userProfile);
+            $users->add($user, true);
+            $this->addFlash('success', 'Your user profile settings were saved.');
+
+            return $this->redirectToRoute('app_settings_profile');
         }
 
         return $this->render('settings_profile/profile.html.twig', [
